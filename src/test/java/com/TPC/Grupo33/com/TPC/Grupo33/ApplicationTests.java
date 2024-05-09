50958d4e1fada0341d02ee2159c1e03b068253f5
Alan <alan_wz12@hotmail.com>
2024-05-08 23:02:50 -0300

    first commit

---
+++ src/test/java/com/TPC/Grupo33/com/TPC/Grupo33/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPC.Grupo33.com.TPC.Grupo33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
